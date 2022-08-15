9b4512cacf96512bffef380a287d997d52275bff
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-08-15 07:35:51 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -56,9 +56,6 @@ class StoryMenuState extends MusicBeatState
 
 	override function create()
 	{
-		Paths.clearStoredMemory();
-		Paths.clearUnusedMemory();
-
 		transIn = FlxTransitionableState.defaultTransIn;
 		transOut = FlxTransitionableState.defaultTransOut;
 
